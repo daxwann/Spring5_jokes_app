727e719fc9e5c8d0f802c085ab65d9b645b4431b
Dax Wann <daxwann@gmail.com>
2020-04-19 15:36:36 -0700

    add ChuckConfiguration

---
+++ src/main/java/dax/springframework/joke/joke_app/config/ChuckConfiguration.java
@@ -0,0 +1,14 @@
+package dax.springframework.joke.joke_app.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckConfiguration {
+
+  @Bean
+  public ChuckNorrisQuotes chuckNorrisQuotes() {
+    return new ChuckNorrisQuotes();
+  }
+}
